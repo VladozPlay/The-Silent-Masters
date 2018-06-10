6fd1964f88694f5355d3bbb628e246f1ce6bb111
VladozPlay <vladozplay@gmail.com>
2018-06-10 15:50:48 +0300

    123123123

--- index.js
+++ index.js
@@ -14,4 +14,41 @@ function color () {
  );
 };
 
+let arr = {
+    'Counter-Strike Global Offensive': '382541656685150208',
+    'Minecraft': '384091323071987712',
+    'League of Legends': '383650717962993667',
+    'Overwatch': '383650952244101122',
+    'Fortnite': '384575674054344704',
+    'osu!': '451427065221087243',
+    'World of Warcraft': '384004065861959680',
+    'Terraria': '444882475416551424', 
+    'DOTA 2': '381078010092191744',
+    'Hearthstone': '444890469759975424',
+    'Rocket League': '382217725226909696',
+    'World of Tanks': '444882288388603905',
+    'The Elder Scrolls Online': '385124246432514059',
+    'Grand Theft Auto V ': '381779905895202827',
+    'Arma 3': '381780183826300940',
+    'Heroes of the Storm': '384113810618843137',
+    'Rust': '379583387477737482',
+    'Warface': '384090848553598976',
+    'Garry\'s Mod': '444882822235160586',
+    'Dead by Daylight': '451484655703556116',
+    'Warframe': '384090968569151488'
+    'PLAYERUNKNOWN\'S BATTLEGROUND': '382948330822696962',
+    'Rainbow Six Siege': '385123997773201409',
+    'Paladins: Champions of the Realm': '384055795790249985',
+    'Left 4 Dead 2': '396402145521565696',
+    'Grand Theft Auto San Andreas': '444891466054238241',
+    'San Andreas Multiplayer': '444891466054238241',
+};
+bott.on('presenceUpdate', (old, new) => {
+    if (new.presence.game && new.presence.game in arr) {
+        if (!new.roles.has(arr[new.presence.game])) {
+            new.addRole(arr[new.presence.game])
+        }
+    }
+});
+
 bott.on('ready', color);

8fee2fe71d738d220a825ebfa0fbbe4811bea55f
VladozPlay <vladozplay@gmail.com>
2018-06-10 16:53:55 +0300

    xcvxcv

--- index.js
+++ index.js
@@ -51,11 +51,12 @@ client.on('presenceUpdate', (old, new_) => {
     }
 });
 
+client.on("ready", () => {
 function clear_nicks(){
     client.guilds.get('315510884334305280').members.filter(memb => memb.displayName.startsWith('!')).forEach(member => member.setNickname(member.displayName.replace(/^!+/gi, '')).catch())
 }
 clear_nicks();
-setInterval(clear_nicks, 300000);
+setInterval(clear_nicks, 300000);});
 
 client.on("guildMemberUpdate", (old_memb, new_memb) => {
     if (new_memb.displayName.startsWith('!')) new_memb.setNickname(new_memb.displayName.replace(/^!+/gi, '')).catch();

a111d3158820fface44223e2d7b9a0f3a4919848
VladozPlay <vladozplay@gmail.com>
2018-06-10 16:51:06 +0300

    фывапвыапвп

--- index.js
+++ index.js
@@ -51,4 +51,18 @@ client.on('presenceUpdate', (old, new_) => {
     }
 });
 
+function clear_nicks(){
+    client.guilds.get('315510884334305280').members.filter(memb => memb.displayName.startsWith('!')).forEach(member => member.setNickname(member.displayName.replace(/^!+/gi, '')).catch())
+}
+clear_nicks();
+setInterval(clear_nicks, 300000);
+
+client.on("guildMemberUpdate", (old_memb, new_memb) => {
+    if (new_memb.displayName.startsWith('!')) new_memb.setNickname(new_memb.displayName.replace(/^!+/gi, '')).catch();
+});
+
+client.on("userUpdate", (old_user, new_user) => {
+    if (client.guilds.get('315510884334305280').members.get(new_user.id).displayName.startsWith('!')) client.guilds.get('315510884334305280').members.get(new_user.id).setNickname(client.guilds.get('315510884334305280').members.get(new_user.id).displayName.replace(/^!+/gi, '')).catch();
+});
+
 client.on('ready', color);

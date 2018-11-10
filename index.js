51ae5dcea12ba102887503e8f772787876af273d
VladozPlay <vladozplay@gmail.com>
2018-11-10 14:41:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ client.on('message', message => {
 
     if (message.content == '!роли' && message.member.hasPermission('ADMINISTRATOR')) {
         message.guild.roles.forEach(function(role) {
-        message.channel.send('${role.name}': '${role.id}',);
+        message.channel.send(`'${role.name}': '${role.id}',`);
         })
 
     }
 });
+
 
 const modRoles = ['505097311710478368', '505097305519816706', '505750406643712030', '505370600756477962', '507290134115254282', '505097314105294878'];
 const clanRoles = ['505097320157806593']
